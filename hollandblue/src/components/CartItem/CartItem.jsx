8db8daec491ac20ad8669de1cb9e8e614cadee01
Francisco Zuidwijk <electrocisko@gmail.com>
2022-05-25 18:29:02 -0300

    se agrego control de stock en Cartitem

--- hollandblue/src/components/CartItem/CartItem.jsx
+++ hollandblue/src/components/CartItem/CartItem.jsx
@@ -5,7 +5,32 @@ import CartContext from '../../Context/cart-context';
 
 function CartItem({item}) {
 
-    const productsInCart = useContext(CartContext)
+    const productsInCart = useContext(CartContext);
+    let controlDeStock = parseInt(localStorage.getItem(item.id));  // Recupero el stock que tiene desde el localstorage
+
+    const removeUnitStorage = ()=>{
+        productsInCart.removeOneUnit(item.id);
+        localStorage.setItem(item.id,controlDeStock+1)
+    }
+
+    //Funcion que verifica si hay stock para agregar
+    const addOneUnitStorage = ()=>{
+        if (controlDeStock > 0){
+            productsInCart.addOneUnit(item);
+            localStorage.setItem(item.id,controlDeStock-1);
+        }
+        else{
+            alert('No hay stock');
+            controlDeStock=0;
+        }   
+    }
+
+    const removerPorId = ()=> {
+        productsInCart.removerPorId(item.id);
+        localStorage.setItem(item.id,controlDeStock + productsInCart.getCartQuantity(item.id));
+    }
+
+
     
     return (
         <div className='cart-container'>
@@ -16,16 +41,16 @@ function CartItem({item}) {
                 <h4 className='producto__cart__title'>{item.title}</h4>
             </div>
            <div className='container-cart-quantity'>
-                <button className='cart-button' onClick={()=>productsInCart.removeOneUnit(item.id)}><i className="fa-solid fa-minus"></i></button>
+                <button className='cart-button' onClick={()=>removeUnitStorage()}><i className="fa-solid fa-minus"></i></button>
                 <p className='producto__cart__quantity'>{item.quantity} U</p>
-                <button className='cart-button' onClick={()=>productsInCart.addOneUnit(item)}><i className="fa-solid fa-plus"></i></button>
+                <button className='cart-button' onClick={()=>addOneUnitStorage()}><i className="fa-solid fa-plus"></i></button>
                 
             </div>
             <div className='container-cart-precio'>
                 <p className='producto__cart__precio'>${(item.price * item.quantity).toLocaleString()} </p>
             </div>  
             <div className='container-trash'>
-                <button className='cart-button' onClick={()=>productsInCart.removerPorId(item.id)}><i className="fa-solid fa-trash"></i></button>
+                <button className='cart-button' onClick={()=>removerPorId()}><i className="fa-solid fa-trash"></i></button>
             </div>  
           
         </div>
